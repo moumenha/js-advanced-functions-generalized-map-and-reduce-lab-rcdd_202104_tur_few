6ee2ce7b45eddf6d1f4a2920840f49e0eddbd8ec
Moumen Hallak <moumen95@gmail.com>
2021-06-20 20:48:41 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Add your functions here
+
+function map(obj, b) {
+  let arr = [];
+  for (let i=0; i <obj.length; i++){
+  arr.push(b(obj[i]));
+}
+return arr;
+}
+
+function reduce(obj, b, acc){
+  const keys = Object.keys(obj);
+  let i = 1;
+  if(acc){
+    i=0;
+  }else{
+    acc = obj[keys[0]];
+  }
+  for (i; i<keys.length; i++ ){
+    acc = b(acc, obj[keys[i]]);
+  }
+  return acc;
+}
+
